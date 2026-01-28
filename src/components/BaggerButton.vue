fd50dbb7f0419697a87214cdcfa4ffbb1a562c24
Rik Smits <rikhjsmits@gmail.com>
2026-01-28 14:31:21 +0100

    Merge pull request #7 from S-D-V-A-S-M-V-V-I-C-T-S-S-BAGGER/feat-bagger-button-component
    Feat bagger button component

---
+++ src/components/BaggerButton.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const emit = defineEmits(['click'])
+
+function emitClickEvent() {
+  emit('click')
+}
+
+defineProps({
+  isPrimary: {
+    type: Boolean,
+    required: false,
+    default: true,
+  },
+})
+</script>
+
+<template>
+  <button
+    @click="emitClickEvent"
+    :class="
+      isPrimary
+        ? 'bg-primary text-black border-primary'
+        : 'bg-secondary text-white border-secondary'
+    "
+    class="border rounded-xl min-w-35 pop-effects"
+  >
+    <slot>🌱</slot>
+  </button>
+</template>
+
+<style scoped>
+button {
+  transition: all 0.4s ease-in-out;
+  transform: scale(1);
+}
+button:hover {
+  transform: scale(1.1);
+}
+</style>
