b299b59f2341240c4f01b69360f1b5ebe8b21ded
Dilan van Oosten <dilanoosten@gmail.com>
2026-02-12 15:16:53 +0100

    feat(turf): #3 completed the styling of the turf lines

--- src/components/TurfLine.vue
+++ src/components/TurfLine.vue
@@ -17,13 +17,14 @@
 </script>
 
 <template>
-  <div id="line" class="grid grid-rows-1 grid-cols-2 gap-y-2 gap-x-2">
+  <div id="line" class="grid grid-rows-1 grid-cols-2 gap-2
+                        md:grid-cols-[15vw_25vw]">
 
     <div id="counter" class="row-start-1 col-start-1 col-span-1
-     grid grid-rows-1 grid-cols-3 gap-x-2 ">
+     grid grid-rows-1 grid-cols-3 gap-x-2 md:grid-cols-[4vw_6vw_4vw]">
 
       <div id="minus" class="row-start-1 col-start-1 col-span-1">
-        <BaggerButton customStyling="w-[100%] h-[100%]" @click="decrease">-</BaggerButton>
+        <BaggerButton :isPrimary=false customStyling="w-[100%] h-[100%]" @click="decrease">-</BaggerButton>
       </div>
 
       <div id="amount" class="row-start-1 row-span-1 col-start-2 col-span-1 h-full">
@@ -35,15 +36,16 @@
       </div>
 
       <div id="plus" class="row-start-1 row-span-1 col-start-3 col-span-1">
-        <BaggerButton customStyling="w-[100%] h-[100%]" @click="increase">+</BaggerButton>
+        <BaggerButton :isPrimary=false customStyling="w-[100%] h-[100%]" @click="increase">+</BaggerButton>
       </div>
 
     </div>
 
     <div id="name-and-price" class="row-start-1
-    grid grid-rows-2 grid-cols-1 gap-y-2">
+    grid grid-rows-2 grid-cols-1 gap-y-2
+      md:grid-rows-1 md:grid-cols-2 md:gap-x-2 ">
 
-      <div id="item" class="row-start-1 row-span-1">
+      <div id="item" class="row-start-1 row-span-1 md:col-start-1 md:col-span-1">
         <BaggerInput
           v-model="item"
           type="text"
@@ -51,7 +53,7 @@
         />
       </div>
 
-      <div id="price" class="row-start-2 row-span-1">
+      <div id="price" class="row-start-2 row-span-1 md:row-start-1 md:col-start-2 md:col-span-1">
         <BaggerInput
           v-model="price"
           type="number"
--- src/views/Turf.vue
+++ src/views/Turf.vue
@@ -16,33 +16,38 @@
 <template>
 
   <!-- Flex -->
-  <div id="wrapper" class="flex flex-col items-center mx-5 my-5 gap-y-5">
+  <div id="wrapper" class="flex flex-col items-center mx-5 my-5 gap-y-5 md:gap-y-8">
 
     <!-- Flex -->
     <div id="total" class="flex flex-col gap-y-1">
-      <BaggerButton customStyling="w-[40vw]">Reset</BaggerButton>
-      <BaggerButton customStyling="w-[40vw]" >Totaal: {{ total }}</BaggerButton>
+      <BaggerButton :isPrimary=false customStyling="w-[40vw] md:w-[15vw]">Reset</BaggerButton>
+      <BaggerButton :isPrimary=false customStyling="w-[40vw] md:w-[15vw]" >Totaal: {{ total }}</BaggerButton>
     </div>
 
     <!-- Grid 2 rows 2 cols -->
-    <div id="titles" class="grid grid-rows-2 grid-cols-2 gap-x-5">
-      <h1 class="row-start-1 row-span-2 col-start-1 col-span-1 self-center">Hoeveel</h1>
-      <h2 class="row-start-1 row-span-1 col-start-2 col-span-1">Wat</h2>
-      <h2 class="row-start-2 row-span-1 col-start-2 col-span-1">Prijs</h2>
+    <div id="titles" class="grid grid-rows-1 grid-cols-2 gap-x-5 w-[80vw] justify-items-center items-center
+                               md:w-[40vw] md:grid-cols-[15vw_25vw]">
+      <h1 class="">Hoeveel</h1>
+      <div id="sub-headers" class="md:grid md:grid-rows-1 md:grid-cols-2 md:w-[100%]">
+        <h2 class="md:row-start-1 md:col-start-1 md:col-span-1">Wat</h2>
+        <h2 class="md:row-start-1 md:col-start-2 md:col-span-1">Prijs</h2>
+      </div>
     </div>
 
-
-    <!-- Grid 2 rows 4 cols -->
-    <div id="turf-lines" class="w-[80vw] flex flex-col gap-y-5 md:w-[70vw]">
+    <div id="turf-lines" class="w-[80vw] flex flex-col gap-y-5 md:w-[40vw]">
       <TurfLine v-for="(line, i) in lines" :key="i" />
     </div>
 
     <div id="plus-turf-line">
-      <BaggerButton customStyling="w-[40vw]" @click="addLine">+</BaggerButton>
+      <BaggerButton :isPrimary=false customStyling="w-[40vw] md:w-[15vw]" @click="addLine">+</BaggerButton>
+    </div>
+
+    <div id="submit">
+      <BaggerButton :isPrimary=false customStyling="w-[40vw] md:w-[15vw]">Klaar!</BaggerButton>
     </div>
 
     <div id="login">
-      <BaggerButton customStyling="w-[40vw]">Login</BaggerButton>
+      <BaggerButton :isPrimary=false customStyling="w-[40vw] md:w-[15vw]">Login</BaggerButton>
     </div>
 
   </div>
