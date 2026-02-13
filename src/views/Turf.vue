5b99e872618ac22414fa668333f2a1030123f022
Dilan van Oosten <dilanoosten@gmail.com>
2026-02-13 11:06:53 +0100

    feat(turf): #3 Made a popup before submitting the turf lines

--- src/views/Turf.vue
+++ src/views/Turf.vue
@@ -13,6 +13,7 @@ interface LineItem {
 const total = ref(0)
 const lines = ref<LineItem[]>([{}])
 const resets = ref(0);
+const popup = ref(false);
 
 function addLine() {
   lines.value.push({})
@@ -39,6 +40,14 @@ function resetTurf() {
   lines.value = [{}];
   total.value = 0;
   resets.value++;
+}
+
+function submit() {
+//   TODO: 20269213 - To add the logic for submitting the TurfLines
+  console.log("Submit: ", lines.value, total.value);
+
+  popup.value = false;
+  resetTurf();
 }
 
 </script>
@@ -80,7 +89,7 @@ function resetTurf() {
     </div>
 
     <div v-if="login" id="submit">
-      <BaggerButton :isPrimary=false customStyling="w-[40vw] md:w-[15vw]">Klaar!</BaggerButton>
+      <BaggerButton :isPrimary=false customStyling="w-[40vw] md:w-[15vw]" @click="popup = true">Klaar!</BaggerButton>
     </div>
 
     <div v-else id="login">
@@ -89,7 +98,23 @@ function resetTurf() {
 
   </div>
 
+<!--  The popup is place outside of the wrapper container-->
+  <div v-if="popup" id="popup-overlay" class="fixed inset-0 bg-black/60 flex items-center justify-center z-50">
+    <div id="popup" class="w-[50vw] h-fit p-5 flex flex-col justify-center items-center gap-y-3 rounded-xl
+                            md:w-[25vw] md:p-5 md:gap-y-5">
+      <p id="question" class="text-wrap text-center">Weet je zeker dat je dit op wilt sturen?</p>
+      <div id="popup-buttons" class="flex flex-row justify-evenly w-[100%]">
+        <BaggerButton :isPrimary=false customStyling="w-[15vw] md:w-[8vw]" @click="popup = false">Nee</BaggerButton>
+        <BaggerButton :isPrimary=false customStyling="w-[15vw] md:w-[8vw]" @click="submit">Ja</BaggerButton>
+      </div>
+    </div>
+  </div>
+
 
 </template>
 
-<style scoped></style>
+<style scoped>
+  #popup {
+    background: var(--color-brown-medium);
+  }
+</style>
