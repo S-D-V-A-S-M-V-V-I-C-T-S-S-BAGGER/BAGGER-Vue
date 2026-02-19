35e84e9fe8165e9ec4b87b7e68c5e88dca3e88d5
Dilan van Oosten <dilanoosten@gmail.com>
2026-02-19 11:15:43 +0100

    feat(bafko): #6 Added mobile styling for the bafko boxes and added the types of bafkos

---
+++ src/components/BafkoBox.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="box_wrapper" class="w-[100%] h-[15vh] flex flex-col p-5 bg-secondary border-primary rounded-xl gap-y-5">
+
+    <div id="title" class="w-[100%] flex flex-row justify-between item-center">
+      <h2 class="text-shadow-md font-bold"><slot name="bafko"></slot></h2>
+      <p class="border py-1 px-3 w-fit rounded-xl text-shadow-lg font-bold"><slot name="type"></slot></p>
+    </div>
+
+    <div id="description">
+      <p class="text-wrap font-semibold text-shadow-md"><slot name="description"></slot></p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/BafkosView.vue
+++ src/views/BafkosView.vue
@@ -3,20 +3,50 @@
 import BaggerButton from '@/components/BaggerButton.vue'
 import BaggerInput from '@/components/BaggerInput.vue'
 import BaggerDropDown from '@/components/BaggerDropDown.vue'
+import BafkoBox from '@/components/BafkoBox.vue'
+import { onMounted, ref } from 'vue'
 
 export interface DropDownOption {
   label: string;
   value: string;
 }
 
-const dummyOptions = (amount: number) => {
-  const result = [];
-  for (let i = 0; i < amount; i++) {
-    const option: DropDownOption = {label: `Option ${i}`, value: `option-${i}`};
-    result.push(option);
-  }
-  return result;
+interface Bafko {
+  bafko: string;
+  type: string;
+  description: string;
 }
+
+const dummy = ref<Bafko[]>([])
+const options = ref<DropDownOption[]>([]);
+
+onMounted(() => {
+  const bafko1: Bafko = { bafko: 'BAfko', type: 'znw', description: 'BAGGER Afkorting (Commissie)' }
+  dummy.value.push(bafko1)
+  const bafko2: Bafko = {
+    bafko: 'GGGOEEEL',
+    type: 'spreuk',
+    description: 'Goud Geel Grolschjes Openen, Echt Echt Echt Lekker'
+  }
+  dummy.value.push(bafko2)
+  const bafko3: Bafko = {
+    bafko: 'Bavaria',
+    type: 'volzin',
+    description: 'BAGGER vakantie richting Antwerpen'
+  }
+  dummy.value.push(bafko3);
+  // also getting the dropdown for options for now
+  const all: DropDownOption = {label: "all", value: "all"};
+  const znw: DropDownOption = {label: "znw", value: "znw"};
+  const spreuk: DropDownOption = {label: "spreuk", value: "spreuk"};
+  const volzin: DropDownOption = {label: "volzin", value: "volzin"};
+  const tussenwerpsel: DropDownOption = {label: "tussenwerpsel", value: "tussenwerpsel"};
+  options.value.push(all);
+  options.value.push(znw);
+  options.value.push(spreuk);
+  options.value.push(volzin);
+  options.value.push(tussenwerpsel);
+});
 
 
 </script>
@@ -26,21 +56,30 @@ const dummyOptions = (amount: number) => {
 
     <div id="title" class="text-wrap text-center">
       <h1>BAFKO's</h1>
-      <h3>BAGGER Afkortingen Die Dan Wel Dan Niet Gemaakt Worden Door Het Woord BAGGER Voor Een Ander Woord Te Zetten</h3>
+      <h3>BAGGER Afkortingen Die Dan Wel Dan Niet Gemaakt Worden Door Het Woord BAGGER Voor Een
+        Ander Woord Te Zetten</h3>
     </div>
 
     <BaggerButton customStyling="w-[80vw] h-[5vh]">Willekeurige BAFKO</BaggerButton>
 
-    <BaggerInput type="text" placeholder="Zoek voor bafko of bescrhijving" customStyling="w-[80vw]" icon="pi pi-search"></BaggerInput>
+    <BaggerInput type="text" placeholder="Zoek voor bafko of bescrhijving" customStyling="w-[80vw]"
+                 icon="pi pi-search"></BaggerInput>
 
     <div id="types" class="flex flex-col items-start w-[80vw]">
       <h4>Type</h4>
-      <BaggerDropDown width="w-[80vw]" :options="dummyOptions(3)"></BaggerDropDown>
+      <BaggerDropDown width="w-[80vw]" :options="options"></BaggerDropDown>
     </div>
 
-    <div>start of list</div>
+    <div id="list"
+         class="flex flex-col items-center w-[80vw] mt-2"
+         v-for="(bafko, index) in dummy" :key="index">
+      <BafkoBox>
+        <template v-slot:bafko>{{ bafko.bafko }}</template>
+        <template v-slot:type>{{ bafko.type }}</template>
+        <template v-slot:description>{{ bafko.description }}</template>
+      </BafkoBox>
+    </div>
 
-    <div>bafko box</div>
 
   </div>
 
