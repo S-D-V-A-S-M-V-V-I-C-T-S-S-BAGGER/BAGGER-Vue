2b34139e44723c6116ff9a6ccf0d76c1029d8cdb
Dilan van Oosten <dilanoosten@gmail.com>
2026-02-24 12:56:09 +0100

    fix(bafko): #6 Fixed issue with the routing of the buttons in the BaggerFooter

--- src/components/BaggerFooter.vue
+++ src/components/BaggerFooter.vue
@@ -6,10 +6,10 @@ import BaggerButton from '@/components/BaggerButton.vue'
 <template>
   <div id="footer-wrapper" class="flex flex-row w-[80vw] max-h-fit items-center justify-evenly pt-5 border-t-1 border-primary">
     <BaggerButton>
-      <router-link to="{ name: 'home'}"><i class="pi pi-home"></i></router-link>
+      <router-link :to="{ name: 'home'}"><i class="pi pi-home"></i></router-link>
     </BaggerButton>
     <BaggerButton>
-      <router-link to="{ name: 'sitemap'}"><i class="pi pi-bars"></i></router-link>
+      <router-link :to="{ name: 'sitemap'}"><i class="pi pi-bars"></i></router-link>
     </BaggerButton>
     <BaggerButton>LOGOUT</BaggerButton>
   </div>
